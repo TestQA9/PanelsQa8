0c8bd1cdb176c02d17e86b808a91ab9eec031b0f
yacov <jakoff@gmail.com>
2017-04-28 12:03:53 +0300

    test class

--- src/test/java/com/telran/LoginExampleTest.java
+++ src/test/java/com/telran/LoginExampleTest.java
@@ -1,19 +1,31 @@
 package com.telran;
 
-import com.telran.pages.LoginIakovPage;
+import com.telran.pages.LoginIakovV2Page;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class LoginExampleTest extends TestNgTestBase {
 
-    public LoginIakovPage loginIakovPage;
+    public LoginIakovV2Page loginIakovV2Page;
 
 
     @BeforeMethod
     public void initPageObjects() {
-        loginIakovPage = PageFactory.initElements(driver, LoginIakovPage.class);
+        loginIakovV2Page = PageFactory.initElements(driver, LoginIakovV2Page.class);
 
     }
 
+    @Test
+    public void negativeLoginTest() {
+        driver.get("https://greengnome.github.io/panels");
+        loginIakovV2Page.waitLoginPagetoload();
+        loginIakovV2Page.fillUsername("");
+        loginIakovV2Page.fillPassword("");
+        loginIakovV2Page.clicktoLogin();
+        loginIakovV2Page.waitLoginPagetoload();
+        Assert.assertTrue(loginIakovV2Page.isOnLoginPage(), "We are not on login page");
+    }
 
 }
