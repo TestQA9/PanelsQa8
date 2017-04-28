e07106d322483bf7c803c87a86dea61fc854a147
Victoria <rogovavv89@gmail.com>
2017-04-28 11:56:14 +0300

    Gleb

---
+++ src/test/java/com/telran/LoginGlebTest.java
@@ -0,0 +1,32 @@
+package com.telran;
+
+import com.telran.pages.LoginGlebPage;
+import com.telran.pages.LoginIakovPage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginGlebTest extends TestNgTestBase {
+
+    public LoginGlebPage loginGlebPage;
+
+
+    @BeforeMethod
+    public void initPageObjects() {
+        loginGlebPage = PageFactory.initElements(driver, LoginGlebPage.class);
+
+    }
+
+    @Test
+public void negativeLoginTest(){
+        driver.get("https://greengnome.github.io/panels/#/login");
+        loginGlebPage.waitLoginPageToLoad();
+        loginGlebPage.fillLoginField("");
+        loginGlebPage.fillPassField("");
+        loginGlebPage.pressLoginButton();
+        loginGlebPage.waitLoginPageToLoad();
+    Assert.assertTrue(loginGlebPage.isOnLoginPage(), "We are not on login page");
+}
+
+}
