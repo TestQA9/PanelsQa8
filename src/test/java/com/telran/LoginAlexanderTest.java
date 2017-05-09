f0af3d306cb088b0ff1a6be41d74ad0332d1532d
Alex <alexshufutinsk@gmail.com>
2017-05-09 18:42:25 +0300

    hhg

---
+++ src/test/java/com/telran/LoginAlexanderTest.java
@@ -0,0 +1,41 @@
+package com.telran;
+
+import com.telran.pages.LoginAlexanderPage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginAlexanderTest extends TestNgTestBase {
+
+    public LoginAlexanderPage loginAlexanderPage;
+
+
+    @BeforeMethod
+    public void initPageObjects() {
+        loginAlexanderPage = PageFactory.initElements(driver, LoginAlexanderPage.class);
+
+    }
+    @Test (groups = {"smoke",})
+    public void negativeLoginTest(){
+        driver.get("https://greengnome.github.io/panels");
+        loginAlexanderPage.waitLoginPageLoaded();
+        loginAlexanderPage.fillUserName(" ");
+        loginAlexanderPage.fillPassword(" ");
+        loginAlexanderPage.preesLoginButton();
+        loginAlexanderPage.waitLoginPageLoaded();
+
+        Assert.assertTrue(loginAlexanderPage.isOnLoginPage(),"We Are Not On Login Page");
+    }
+
+    @Test (groups = {"inProgress"})
+    public void positiveLoginTest(){
+        driver.get("https://greengnome.github.io/panels");
+        loginAlexanderPage.waitLoginPageLoaded();
+        loginAlexanderPage.fillUserName(" ");
+        loginAlexanderPage.fillPassword(" ");
+        loginAlexanderPage.preesLoginButton();
+        loginAlexanderPage.waitLoginPageLoaded();
+    }
+
+}
